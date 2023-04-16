9b57b9cafa751255aa8438768d529343b7cd9c86
Amirhosseinnk81 <96089096+Amirhosseinnk81@users.noreply.github.com>
2023-04-16 16:10:18 -0700

    add login templates to project

--- weblog_project/settings.py
+++ weblog_project/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #my app
-    'home_app.apps.HomeAppConfig'
+    'home_app.apps.HomeAppConfig',
+    'account_app.apps.AccountAppConfig'
 ]
 
 MIDDLEWARE = [
@@ -120,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR , 'assets')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR , 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
