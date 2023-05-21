6ca4e678c00a8470f7bf82ed7200442c129b1cf6
Amirhosseinnk81 <96089096+Amirhosseinnk81@users.noreply.github.com>
2023-05-21 16:00:11 -0700

    edited login

--- weblog_project/urls.py
+++ weblog_project/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home_app.urls')),
     path('', include('account_app.urls')),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
